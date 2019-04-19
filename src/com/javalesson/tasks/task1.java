30cff016afb25819d9538878e850b7b35cea3f5d
Anastasiya Ozherelyeva <aozherelyeva@gmail.com>
2019-04-19 18:14:20 +0300

    Solve 2 simple tasks

--- src/com/javalesson/tasks/task1.java
+++ src/com/javalesson/tasks/task1.java
@@ -4,7 +4,7 @@ package com.javalesson.tasks;
 
 public class task1 {
     public static void main(String[] args) {
-        for (int counter=1; counter<100; counter++) {
+        for (int counter = 1; counter < 100; counter++) {
             System.out.println(counter);
         }
     }
