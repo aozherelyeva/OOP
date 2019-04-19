b624be23c1f102d515ba1f602e0c9ff5256bd9d3
Anastasiya Ozherelyeva <aozherelyeva@gmail.com>
2019-04-19 18:13:54 +0300

    Solve 2 simple tasks

--- src/com/javalesson/tasks/task2.java
+++ src/com/javalesson/tasks/task2.java
@@ -2,5 +2,21 @@
 
 package com.javalesson.tasks;
 
+import java.util.Scanner;
+
 public class task2 {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter a number");
+
+        String number = myObj.nextLine();  // Read user input
+        int input = Integer.parseInt(number);
+        int output = 1;
+
+        for (int i = 1; i <= input; i++) {
+            output *= i;
+        }
+
+        System.out.println(input+" factorial is "+output);
+    }
 }
