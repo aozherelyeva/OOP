712ef23666326ba5ddf416c501d95358b1258a56
aozherelyeva <aozherelyeva@gmail.com>
2019-04-20 23:34:28 +0300

    Resolve task 5

--- src/com/javalesson/tasks/task5.java
+++ src/com/javalesson/tasks/task5.java
@@ -2,5 +2,20 @@
 
 package com.javalesson.tasks;
 
+import java.util.Scanner;
+
 public class task5 {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Enter a number of the -5's that you want to see:");
+        String num = myObj.nextLine();  // Read user input
+
+        int number = Integer.parseInt(num);
+        int initial = -5;
+
+        for (int i = 0; i <= number; i++) {
+            System.out.println(initial * i);
+        }
+
+    }
 }
