5e181d5f0279df523cf5d608eb88ae2955e78896
aozherelyeva <aozherelyeva@gmail.com>
2019-04-21 00:36:33 +0300

    Resolve task 6

--- src/com/javalesson/tasks/task6.java
+++ src/com/javalesson/tasks/task6.java
@@ -1,38 +1,61 @@
 //Переделайте последний пример на break.Необходимо, чтобы заданный символ встречался в строке хотя бы 2 раза.
+package com.javalesson.tasks;
 
-/*public class BreakDemo {
+import java.util.Scanner;
+
+public class task6 {
 
     public static void main(String[] args) {
 
-        //строка для поиска
-        String searchMe = "Мама мыла раму";
-        //количество символов в строке
-        int max = searchMe.length();
-        //символ, который нужно найти в строке
-        char symb = 'ы';
-        //переключатель найдено/не найдено
-        boolean find = false;
+        Scanner myObj = new Scanner(System.in);
+        System.out.println("Введите исходный текст");
+        String searchMe = myObj.nextLine();  // Read user input
 
-        for (int i = 0; i < max; i++) {
-            // если символ на позиции i в строке равен искомому символу
-            if (searchMe.charAt(i) == symb) {
-                //переключаемся в режим "найдено" и выходим из цикла
-                find = true;
-                break;
+        int max = searchMe.length();
+
+        char symb = 0;
+
+        Scanner myObj2 = new Scanner(System.in);
+        System.out.println("Введите символ для вхождения");
+        String symbolIn = myObj2.nextLine();  // Read user input
+        if (symbolIn.length() > 1) {
+            System.out.println("Введите только один символ!");
+        } else if (symbolIn.length() == 0) {
+            System.out.println("Вы не ввели ни одного символа. Введите один символ");
+        } else {
+            symb = symbolIn.charAt(0);
+
+            System.out.println("....");
+            System.out.println("....");
+            System.out.println("....");
+            System.out.println("...Ведётся поиск 2 вхождений символа " + symb + " в строке " + searchMe + "...");
+            System.out.println("....");
+            System.out.println("....");
+            System.out.println("....\n");
+
+
+            boolean findFirst = false;
+            boolean findSecond = false;
+            int symbol = 0;
+
+            for (int i = 0; i < max; i++) {
+                if (searchMe.charAt(i) == symb) {
+                    findFirst = true;
+                    symbol = i;
+                    break;
+                }
             }
 
+            for (int j = symbol + 1; j < max; j++) {
+                if (searchMe.charAt(j) == symb) {
+                    findSecond = true;
+                    break;
+                }
+            }
+            if (findFirst && findSecond)
+                System.out.println("Символ '" + symb + "' найден в строке как минимум 2 раза");
+            else
+                System.out.println("Символ '" + symb + "' не найден в строке или найден менее 2 раз");
         }
-
-        //выводим соответствующее сообщение: найден или не найден символ
-        if (find)
-            System.out.println("Символ '" + symb + "' найден в строке");
-        else
-            System.out.println("Символ '" + symb + "' не найден в строке");
-
     }
-}*/
-
-package com.javalesson.tasks;
-
-public class task6 {
 }
